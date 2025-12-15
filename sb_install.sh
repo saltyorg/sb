9e906a85357d87eb6cb70cef55ea497a26eb8eab
saltydk <salty@saltbox.dev>
2025-12-15 08:23:53 +0100

    use sb-go for install

--- sb_install.sh
+++ sb_install.sh
@@ -17,8 +17,7 @@ VERBOSE=false
 VERBOSE_OPT=""
 SB_REPO="https://github.com/saltyorg/sb.git"
 SB_PATH="/srv/git/sb"
-RELEASE_FILE="/srv/git/sb/release.txt"
-TARGET_BINARY_PATH="/srv/git/sb/sb"
+TARGET_BINARY_PATH="/usr/local/bin/sb"
 SB_INSTALL_SCRIPT="$SB_PATH/sb_install.sh"
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 BRANCH="master"
@@ -49,7 +48,7 @@ run_cmd() {
 }
 
 download_binary() {
-    local github_tag
+    local api_url
     local version
     local download_url
     local temp_binary_path
@@ -60,24 +59,17 @@ download_binary() {
         run_cmd sudo apt-get install -y file
     fi
 
-    if [ ! -f "${RELEASE_FILE}" ]; then
-        echo "Error: ${RELEASE_FILE} does not exist." >&2
-        exit 1
-    fi
+    api_url="https://api.github.com/repos/saltyorg/sb-go/releases/latest"
 
-    github_tag=$(head -n 1 "${RELEASE_FILE}" | tr -d '[:space:]')
-    if [[ ! "$github_tag" =~ ^refs/tags/ ]]; then
-        echo "Error: Invalid tag format in ${RELEASE_FILE}." >&2
-        exit 1
-    fi
-
-    version=${github_tag#refs/tags/}
+    version=$(curl -s "${api_url}" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
     if [ -z "$version" ]; then
-        echo "Error: No version found in tag $github_tag." >&2
+        echo "Error: Could not determine latest version from GitHub API." >&2
         exit 1
     fi
 
-    download_url="https://github.com/saltyorg/sb/releases/download/$version/sb"
+    echo "Latest sb-go version: $version"
+
+    download_url="https://github.com/saltyorg/sb-go/releases/download/${version}/sb_linux_amd64"
 
     temp_binary_path="${TARGET_BINARY_PATH}.tmp"
     run_cmd curl -L -o "${temp_binary_path}" "${download_url}"
@@ -111,128 +103,18 @@ done
 # Main
 ################################
 
-# Check if Cloudbox is installed
-# develop
-if [ -d "/srv/git/cloudbox" ]; then
-    echo "==== Cloudbox Install Detected ===="
-    echo "Cloudbox installed. Exiting..."
-    echo "==== Cloudbox Install Detected ===="
-    exit 1
-fi
-
-# master
-for directory in /home/*/*/ ; do
-    base=$(basename "$directory")
-    if [ "$base" == "cloudbox" ]; then
-        echo "==== Cloudbox Install Detected ===="
-        echo "Cloudbox installed. Exiting..."
-        echo "==== Cloudbox Install Detected ===="
-        exit 1
-    fi
-done
-
-# Check for supported Ubuntu Releases
-release=$(lsb_release -cs 2>/dev/null | grep -v "No LSB modules are available.")
-
-# Add more releases like (focal|jammy)$
-if [[ $release =~ (jammy|noble)$ ]]; then
-    echo "${release^} is currently supported."
-else
-    echo "==== UNSUPPORTED OS ===="
-    echo "Install cancelled: ${release^} is not supported."
-    echo "Supported OS: 22.04 (Jammy) and 24.04 (Nobel)"
-    echo "==== UNSUPPORTED OS ===="
-    exit 1
-fi
-
-# Check if using valid arch
-arch=$(uname -m)
-
-if [[ $arch =~ (x86_64)$ ]]; then
-    echo "$arch is currently supported."
-else
-    echo "==== UNSUPPORTED CPU Architecture ===="
-    echo "Install cancelled: $arch is not supported."
-    echo "Supported CPU Architecture(s): x86_64"
-    echo "==== UNSUPPORTED CPU Architecture ===="
-    exit 1
-fi
-
-# Check for LXC using systemd-detect-virt
-if systemd-detect-virt -c | grep -qi 'lxc'; then
-    echo "==== UNSUPPORTED VIRTUALIZATION ===="
-    echo "Install cancelled: Running in an LXC container is not supported."
-    echo "==== UNSUPPORTED VIRTUALIZATION ===="
-    exit 1
-fi
-
-# Check if specific desktop packages are installed
-if dpkg -l ubuntu-desktop &>/dev/null; then
-    echo "==== UNSUPPORTED DESKTOP INSTALL ===="
-    echo "Install cancelled: Only Ubuntu Server is supported."
-    echo "==== UNSUPPORTED DESKTOP INSTALL ===="
-    exit 1
-fi
-
-# Define required CPU features for x86-64-v2
-#required_features=("sse4_2" "popcnt")
-
-# Check for x86-64-v2 support
-#for feature in "${required_features[@]}"; do
-#  if ! grep -q " $feature " /proc/cpuinfo; then
-#    echo "==== UNSUPPORTED CPU Microarchitecture ===="
-#    echo "Install cancelled: CPU does not support minimum microarchitecture level: x86-64-v2"
-#    echo "==== UNSUPPORTED CPU Microarchitecture ===="
-#    exit 1
-#  fi
-#done
-
-echo "Installing Saltbox Dependencies."
-
-$VERBOSE && echo "Script Path: $SCRIPT_PATH"
-
 # Update apt cache
 run_cmd apt-get update
 
-# Install git
-run_cmd apt-get install -y git curl
-
-# Remove existing repo folder
-if [ -d "$SB_PATH" ]; then
-    run_cmd rm -rf $SB_PATH;
-fi
-
-# Clone SB repo
-run_cmd mkdir -p /srv/git
-run_cmd mkdir -p /srv/ansible
-run_cmd git clone --branch master "${SB_REPO}" "$SB_PATH"
-
-download_binary
-
-# Set chmod +x on script files
-run_cmd chmod +x $SB_PATH/*.sh
-
-$VERBOSE && echo "Script Path: $SCRIPT_PATH"
-$VERBOSE && echo "SB Install Path: "$SB_INSTALL_SCRIPT
+# Install curl
+run_cmd apt-get install -y curl
 
 # Check if /usr/local/bin exists, create it if not
 if [ ! -d "/usr/local/bin" ]; then
     run_cmd mkdir -m 0755 -p /usr/local/bin
 fi
 
-## Create script symlinks in /usr/local/bin
-shopt -s nullglob
-for i in "$SB_PATH"/*.sh; do
-    if [ ! -f "/usr/local/bin/$(basename "${i%.*}")" ]; then
-        run_cmd ln -s "${i}" "/usr/local/bin/$(basename "${i%.*}")"
-    fi
-done
-shopt -u nullglob
+download_binary
 
-# Install Saltbox Dependencies
-run_cmd bash -H $SB_PATH/sb_dep.sh $VERBOSE_OPT
-
-# Clone Saltbox Repo
-run_cmd bash -H $SB_PATH/sb_repo.sh -b "${BRANCH}" $VERBOSE_OPT
-
-echo "Saltbox Dependencies were successfully installed."
+# Run sb setup
+run_cmd /usr/local/bin/sb setup $VERBOSE_OPT
